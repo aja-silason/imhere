48215449232720ca6ae8911726296f31fd249869
Anania Augusto <anania.augusto@baxtterangola.com>
2024-08-03 22:36:46 +0100

    wip: add feat to add new participant and remove participant, create a custum hook to create and remove participant

---
+++ src/hooks/useCreateParticipant.ts
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { Alert } from "react-native";
+
+const useCreateParticipant = () => {
+
+
+    const [participants, setParticipant] = useState(["Abel"]);
+
+    const [participantName, setParticipantName] = useState<string>("");
+
+
+
+    const handleChangeParticipantName = (value: string) => {
+        setParticipantName(value);
+    }
+
+    function handleParticipantAdd(){ 
+        if(participants.includes(participantName)){
+            return Alert.alert("Aviso", "Usuário já existente");
+        }
+        setParticipant( prevState => [...prevState, participantName]);
+    }
+
+    function handleParticipantRemove(name: string){
+        Alert.alert("Aviso", `Remover o participante ${name}`, [
+            {
+                text: "Sim",
+                onPress: () => {
+                    setParticipant(participants.filter((participant: string) => participant !== name));
+                    Alert.alert("Aviso!", "Usuário removido da lista!")
+                },
+            },
+            {
+                text: "Não",
+                style: "cancel"
+            }
+        ]);
+    }
+
+    return {participantName, participants, handleChangeParticipantName, handleParticipantAdd, handleParticipantRemove }
+
+}
+
+export default useCreateParticipant;
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,30 +1,12 @@
 import { Participant } from "@/src/components/Participant";
 import { styles } from "./styles";
 import { Alert, FlatList, ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { useRef, useState } from "react";
+import useCreateParticipant from "@/src/hooks/useCreateParticipant";
 
 export function Home(){
 
-    const participants = ["Anania", "Silas", "Nádia", "Nando", "Bernardo", "Mãe Zinha", "Kely", "Mamã Rebeca"]
-
-    function handleParticipantAdd(){ 
-        if(participants.includes("Anania")){
-            return Alert.alert("Aviso", "Usuário já existente");
-        }
-        Alert.alert("Atencao", "Voce adicionou participante...");
-    }
-
-    function handleParticipantRemove(name: string){
-        Alert.alert("Aviso", `Remover o participante ${name}`, [
-            {
-                text: "Sim",
-                onPress: () => Alert.alert("Aviso!", "Usuário deletado!"),
-            },
-            {
-                text: "Não",
-                style: "cancel"
-            }
-        ]);
-    }
+    const {handleChangeParticipantName, handleParticipantAdd, handleParticipantRemove, participantName, participants} = useCreateParticipant();
 
     return (
         <View style={styles.container}>
@@ -33,9 +15,11 @@ export function Home(){
 
             <View style={styles.inputContainer}>
                 <TextInput
-                style={styles.input}
-                placeholder="Nome do Participante"
-                placeholderTextColor="#6b6b6b" 
+                    style={styles.input}
+                    placeholder="Nome do Participante"
+                    placeholderTextColor="#6b6b6b"
+                    onChangeText={handleChangeParticipantName} 
+                    value={participantName}
                 />
 
                 <TouchableOpacity activeOpacity={0.7} style={styles.button} onPress={handleParticipantAdd}>
