bc79cbeb9c834c241fdbd209f202e2484dc2110b
Anania Augusto <anania.augusto@baxtterangola.com>
2024-08-03 18:42:37 +0100

    wip: separate style in other file to make the code more legible

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,18 +1,11 @@
-import { StatusBar, StyleSheet, Text, View } from "react-native";
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center"
-    }
-});
+import { styles } from "@/assets/styles/style";
+import { Text, View } from "react-native";
 
 export default function Index(){
     return (
         <View style={styles.container}>
-            <Text>Nova tela na Shit toda</Text>
-            <StatusBar barStyle="dark-content"/>
+            <Text style={styles.text1}>Nome do Evento</Text>
+            <Text style={styles.text2}>Sexta, 4 de Novembro de 2022.</Text>
         </View>
     )
 }
---
+++ src/assets/styles/style.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 24,
+        backgroundColor: "#131016",
+    },
+    text1: {
+        color: "#fdfcfe",
+        fontSize: 24,
+        fontWeight: "bold",
+        marginTop: 48
+    },
+    text2: {
+        color: "#6b6b6b",
+        fontSize: 16,
+    }
+});
