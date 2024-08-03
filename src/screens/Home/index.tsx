3e8392e3fe6e05ea93149b9f509551dabb492bd0
Anania Augusto <anania.augusto@baxtterangola.com>
2024-08-03 18:58:09 +0100

    wip: separeted and divided the project structure

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import { styles } from "./styles";
+import { Text, View } from "react-native";
+
+export default function Home(){
+    return (
+        <View style={styles.container}>
+            <Text style={styles.eventName}>Nome do Evento</Text>
+            <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
+        </View>
+    )
+}
